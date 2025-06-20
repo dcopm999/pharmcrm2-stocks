8fc45635e07e1421ffc6084cf865d48201fbc2d7
Павел Таньчев <dcopm999@gmail.com>
2025-06-20 20:25:16 +0500

    * tests/test_models.py: Added StockCase

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,6 +9,11 @@ Tests for `pharmcrm2-stocks` models module.
 """
 
 from django.test import TestCase
+from django.urls import reverse
+from slugify import slugify
+
+from stocks import models
+from tests import factories
 
 
 class ModelsCase(TestCase):
@@ -20,3 +25,29 @@ class ModelsCase(TestCase):
 
     def tearDown(self):
         pass
+
+
+class BatchItemCase(TestCase):
+    def setUp(self):
+        self.item = factories.StockFactory()
+
+    def test_save_slug(self):
+        self.assertEqual(self.item.slug, slugify(self.item.name))
+
+    def test_str(self):
+        query = models.Stock.objects.last()
+        self.assertEqual(query.__str__(), query.name)
+
+    def test_get_absolute_url(self):
+        query = models.Stock.objects.last()
+        self.assertEqual(
+            query.get_absolute_url(),
+            reverse("stocks:stock-detail", args=[str(query.slug)]),
+        )
+
+
+"""
+class BatchItemCase(TestCase):
+    def setUp(self):
+        query = factories.BatchItemFactory()
+"""
